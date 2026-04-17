922680b325e0ab80bfcfdf4c835c520e98a4caee
Edson Rodrigues <edsonr065@gmail.com>
2026-04-17 10:09:21 -0300

    Refatoração do projeto e configuração de ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -6,5 +6,13 @@ public static class Configuration
     public static string JwtKey = "YTk4NzY1NDMyMWFiY2RlZjEyMzQ1Njc4OWFiY2Rl";
     public static string ApiKeyName  = "api_key";
     public static string ApiKey  = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+    public static SmtpConfiguration Smtp = new();
 
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,44 +4,71 @@ using Blog.Data;
 using Blog.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+ConfigureAuthentication(builder);
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
-builder.Services.AddControllers()
+var app = builder.Build();
+
+LoadConfiguration(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+// Paramos no #F0447
+
+void LoadConfiguration(WebApplication app)
+{
+    
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("Smtp").Bind(smtp);
+    Configuration.Smtp = smtp;
+
+}
+
+void ConfigureAuthentication(WebApplicationBuilder app)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options =>
     {
         options.SuppressModelStateInvalidFilter = true;
     });
+}
 
-builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddTransient<TokenService>();
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<BlogDataContext>();
+    builder.Services.AddTransient<TokenService>();
+}
 
-
-var app = builder.Build();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-// Paramos no #F0442
